96729a6d1ca8b75b9306f3bc4165cda4d5172c37
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-03-03 13:41:34 +0800

    Added UserStatRepository

---
+++ backend/src/main/java/com/rho/backend/repository/UserStatRepository.java
@@ -0,0 +1,10 @@
+package com.rho.backend.repository;
+
+import com.rho.backend.model.UserStat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserStatRepository extends JpaRepository<UserStat, Long> {
+
+}
