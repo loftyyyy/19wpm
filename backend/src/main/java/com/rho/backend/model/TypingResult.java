d659ed6a05b5b8af3689b21c0acfbfcdf37dd881
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-03-03 13:36:34 +0800

    Added TypingResult model

---
+++ backend/src/main/java/com/rho/backend/model/TypingResult.java
@@ -0,0 +1,47 @@
+package com.rho.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "typing_results")
+@Entity
+public class TypingResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "typing_result_id")
+    private Long typingResultId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "text_id", nullable = false)
+    private Long textId;
+
+    @Column(name = "finished_at", nullable = false)
+    private Date finishedAt;
+
+    @Column(name = "duration_ms", nullable = false)
+    private Long durationMs;
+
+    @Column(name = "time_constraint_ms", nullable = false)
+    private Long timeConstraintMs;
+
+    private Double wpm;
+
+    private Double accuracy;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
