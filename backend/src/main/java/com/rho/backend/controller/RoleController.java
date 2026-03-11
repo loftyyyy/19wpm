bd11dc5e05299d70b93a0a3bae7972004e46be80
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-03-11 23:34:41 +0800

    Started on creating skeleton methods for the controller

---
+++ backend/src/main/java/com/rho/backend/controller/RoleController.java
@@ -0,0 +1,9 @@
+package com.rho.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoleController {
+
+}
