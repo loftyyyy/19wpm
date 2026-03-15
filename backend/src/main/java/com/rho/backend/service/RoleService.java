75dcc0f6c70256f187c25f0521ca6da66da4f8eb
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-03-15 10:53:16 +0800

    Started the role MVC

--- backend/src/main/java/com/rho/backend/service/RoleService.java
+++ backend/src/main/java/com/rho/backend/service/RoleService.java
@@ -1,7 +1,21 @@
 package com.rho.backend.service;
 
+import com.rho.backend.dto.role.RoleResponseDTO;
+import com.rho.backend.model.Role;
+import com.rho.backend.repository.RoleRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository){
+        this.roleRepository = roleRepository;
+    }
+
+    public RoleResponseDTO getRoleById(Long id){
+        Role role = roleRepository.getRoleById(id);
+        return new RoleResponseDTO(role);
+    }
 }
