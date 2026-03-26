3e49747c9dcc9ed64dcaa3cd55a63441af9db58b
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-03-26 20:30:24 +0800

    Finished migrating record DTO's for user related DTOs

--- backend/src/main/java/com/rho/backend/dto/role/RoleResponseDTO.java
+++ backend/src/main/java/com/rho/backend/dto/role/RoleResponseDTO.java
@@ -1,20 +1,15 @@
 package com.rho.backend.dto.role;
 
 import com.rho.backend.model.Role;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@Getter
-public class RoleResponseDTO {
-
-    private long roleId;
-    private String roleName;
-
+public record RoleResponseDTO (
+    long roleId,
+    String roleName
+){
     public RoleResponseDTO(Role role){
-        this.roleId = role.getId();
-        this.roleName = role.getName();
+        this(
+                role.getId(),
+                role.getName()
+        );
     }
-
-
 }
