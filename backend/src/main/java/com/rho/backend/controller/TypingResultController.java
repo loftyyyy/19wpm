405daf3c715408fe4f76f288f9a123ab1a0b0927
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-04-29 16:59:22 +0800

    Initial TypingResultController layout

--- backend/src/main/java/com/rho/backend/controller/TypingResultController.java
+++ backend/src/main/java/com/rho/backend/controller/TypingResultController.java
@@ -1,8 +1,19 @@
 package com.rho.backend.controller;
 
+import com.rho.backend.service.TypingResultService;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/v1/result")
 public class TypingResultController {
+
+    private final TypingResultService typingResultService;
+
+    public TypingResultController(TypingResultService typingResultService){
+        this.typingResultService = typingResultService;
+    }
+
+
+
+
 
 }
