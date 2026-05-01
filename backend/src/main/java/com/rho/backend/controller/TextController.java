80c87a53b1b031bfc503b291aff93b78038b0751
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-05-01 21:59:42 +0800

    Initial Controller for Text

--- backend/src/main/java/com/rho/backend/controller/TextController.java
+++ backend/src/main/java/com/rho/backend/controller/TextController.java
@@ -2,14 +2,15 @@ package com.rho.backend.controller;
 
 import com.rho.backend.config.CustomUserDetails;
 import com.rho.backend.dto.text.request.TextRequestDTO;
+import com.rho.backend.dto.text.request.TextUpdateRequestDTO;
 import com.rho.backend.dto.text.response.TextResponseDTO;
 import com.rho.backend.service.TextService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("/api/v1/texts")
 public class TextController {
@@ -20,14 +21,38 @@ public class TextController {
         this.textService = textService;
     }
 
+    @GetMapping("/{textId}")
+    public ResponseEntity<TextResponseDTO> getTextById(@PathVariable long textId, @AuthenticationPrincipal CustomUserDetails customUserDetails){
+        return ResponseEntity.ok(textService.getTextById(textId, customUserDetails.getUserId()));
+    }
+
+    @GetMapping("/preset-texts")
+    public ResponseEntity<List<TextResponseDTO>> getPresetTexts(){
+        List<TextResponseDTO> presetTexts = textService.getPresetTexts();
+        return ResponseEntity.ok(presetTexts);
+    }
+
+    @GetMapping("/user-texts")
+    public ResponseEntity<List<TextResponseDTO>> getUserTexts(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        List<TextResponseDTO> userTexts = textService.getUserTexts(customUserDetails.getUserId());
+        return ResponseEntity.ok(userTexts);
+    }
+
+
     @PostMapping("/custom-text")
     public ResponseEntity<TextResponseDTO> createCustomText(@Valid @RequestBody TextRequestDTO textRequestDTO, @AuthenticationPrincipal CustomUserDetails customUserDetails){
         return ResponseEntity.ok(textService.saveCustomText(textRequestDTO, customUserDetails.getUserId()));
     }
 
-    @PostMapping("text")
+
+    @PostMapping("/text")
     public ResponseEntity<TextResponseDTO> createText(@Valid @RequestBody TextRequestDTO textRequestDTO, @AuthenticationPrincipal CustomUserDetails customUserDetails){
         return ResponseEntity.ok(textService.saveText(textRequestDTO, customUserDetails.getUserId()));
+    }
+
+    @PostMapping("/{textId}")
+    public ResponseEntity<TextResponseDTO> modifyText(@Valid @RequestBody TextUpdateRequestDTO textUpdateRequestDTO, @AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable long textId){
+        return ResponseEntity.ok(textService.modifyText(textUpdateRequestDTO, customUserDetails.getUserId(), textId));
     }
 
 
--- backend/src/main/java/com/rho/backend/service/TextService.java
+++ backend/src/main/java/com/rho/backend/service/TextService.java
@@ -121,5 +121,4 @@ public class TextService {
         return texts.stream().map(TextResponseDTO::new).toList();
     }
 
-
 }
