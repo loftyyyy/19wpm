6dc95eabe0f6a91b91496582b7187524757c4598
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-05-16 20:25:29 +0800

    Implemented OAuth2 for google and github

---
+++ backend/src/main/java/com/rho/backend/config/CustomOAuth2User.java
@@ -0,0 +1,38 @@
+package com.rho.backend.config;
+
+import com.rho.backend.model.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+
+    @Getter
+    private final User user;
+    private final Map<String, Object> attributes;
+
+    public CustomOAuth2User(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName()));
+    }
+
+    @Override
+    public String getName() {
+        return user.getEmail();
+    }
+}
