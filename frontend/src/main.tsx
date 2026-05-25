2ed38fdf89fbd8637cab8fa2242fcaba4d62a41e
yoshbatula <yoshbatula2@gmail.com>
2026-05-25 15:07:12 +0800

    done making responsive the navigation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Dashboard/App.tsx'
+import App from './Dashboard/Dashboard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
