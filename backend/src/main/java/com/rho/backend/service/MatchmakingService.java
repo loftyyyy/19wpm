74121a54390b8d7eb7f76273e654c75ba0f5a9af
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-07-06 23:54:15 +0800

    feat: finished designing the joinQueue method for the race feature

--- backend/src/main/java/com/rho/backend/service/MatchmakingService.java
+++ backend/src/main/java/com/rho/backend/service/MatchmakingService.java
@@ -3,20 +3,31 @@ package com.rho.backend.service;
 import com.rho.backend.enums.TextType;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.time.Duration;
+
 public class MatchmakingService {
     private final RedisTemplate<String, String> redisTemplate;
-    private final String queueTemplate = "matchmaking:queue:";
-    private final String playerTemplate = "matchmaking:player:";
+    private static final String QUEUE_KEY_PREFIX  = "matchmaking:queue:";
+    private static final String PLAYER_KEY_PREFIX = "matchmaking:player:";
 
     public MatchmakingService(RedisTemplate<String, String> redisTemplate){
         this.redisTemplate = redisTemplate;
     }
 
     public void joinQueue(Long userId, TextType textType){
+        String playerKey = PLAYER_KEY_PREFIX + userId.toString();
+        String queueKey =  QUEUE_KEY_PREFIX + textType.name();
 
+        Boolean hasKey = redisTemplate.hasKey(playerKey);
+        if(Boolean.TRUE.equals(hasKey)){
+            throw new IllegalStateException("Player " + userId + " is already in a matchmaking queue.");
+        }
+
+        redisTemplate.opsForList().leftPush(queueKey, userId.toString());
+        redisTemplate.opsForValue().set(playerKey, textType.name(), Duration.ofSeconds(60));
     }
 
-    public void leaveQueue(Long userId){
+    public void leaveQueue(){
 
     }
 
@@ -25,10 +36,10 @@ public class MatchmakingService {
     }
 
     private String queueKey(TextType textType) {
-        return "matchmaking:queue:" + textType.name();
+        return QUEUE_KEY_PREFIX + textType.name();
     }
 
     private String playerKey(Long userId) {
-        return "matchmaking:player:" + userId;
+        return PLAYER_KEY_PREFIX + userId;
     }
 }
